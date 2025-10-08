f28a6ca7bd0e64d6a623f331135e6bdca85654e5
Bruno Jauad <brunojauad26@icloud.com>
2025-10-08 12:04:03 +0100

    empty list test completed

--- CheckoutTest.java
+++ CheckoutTest.java
@@ -1,4 +1,20 @@
-import junit.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.*;
+
 public class CheckoutTest {
+    @Test
+    public void testReturnIfZero(){
+        //Arrange
+        Checkout checkout = new Checkout();
+        List<String> items = new ArrayList<>();
+        //Act
+        String result = checkout.calculateFruits(items);
+        //Assert
+        assertEquals("£0.00", result);
+    }
     
 }
