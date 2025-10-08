25297c30d43b5c024e8ceba5b5b868a00ab05ea3
Bruno Jauad <brunojauad26@icloud.com>
2025-10-08 12:26:31 +0100

    test return price if one orange

--- Checkout.java
+++ Checkout.java
@@ -9,6 +9,8 @@ public class Checkout {
         for(int i = 0; i < items.size(); i++){
             if(items.get(i).equals("Apple")){
                 return "£0.60";
+            }else if(items.get(i).equals("Orange")){
+                return "£0.25";
             }
         }
         return "£0.00";
